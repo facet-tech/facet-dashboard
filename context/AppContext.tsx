235d0039d026402e1e805b6e4882c756c671ed73
mkotsollaris <mkotsollari@gmail.com>
2021-02-19 22:20:21 -0800

    fix build

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { authState as authStateConstant, color, snackbar } from '../shared/constant';
 
-const AppContext = React.createContext({});
+export interface AppState {
+    currAuthState: string;
+    setCurrAuthState: undefined,
+    isUserLoggedIn: false,
+    setIsUserLoggedIn: undefined,
+}
+
+const AppContext = React.createContext({
+    currAuthState: authStateConstant.notSignedIn,
+    setCurrAuthState: undefined,
+    isUserLoggedIn: false,
+    setIsUserLoggedIn: undefined,
+});
 
 export default AppContext;
--- pages/authentication.tsx
+++ pages/authentication.tsx
@@ -3,16 +3,17 @@ import SignIn from '../components/authentication/SignIn'
 import AppProvider from '../context/AppProvider';
 import PageProvider from '../shared/PageProvider';
 import { authState, authState as authStateConstant } from '../shared/constant';
-import AppContext from '../context/AppContext';
 import Admin from '../layouts/Admin'
 import Signup from '../components/authentication/Signup';
 import ConfirmationCode from '../components/authentication/ConfirmationCode';
 import ForgotPassword from '../components/authentication/ForgotPassword';
 import PasswordReset from '../components/authentication/PasswordReset';
 import Dashboard from './dashboard';
+import AppContext from '../context/AppContext';
 
 const Authentication = () => {
-    const { currAuthState, jwt, setCurrAuthState } = useContext(AppContext);
+
+    const { currAuthState } = useContext(AppContext);
 
     let displayElement;
     if (currAuthState === authStateConstant.signedIn) {
