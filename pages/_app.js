240ebf7a7f9fb632cb9c1a52cc6a9ba465e37605
mkotsollaris <mkotsollari@gmail.com>
2021-02-23 11:58:11 -0800

    save progress

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,16 @@ export default class MyApp extends App {
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        {/* make height full */}
+        <style global jsx>{`
+                html,
+                body,
+                body > div:first-child,
+                div#__next,
+                div#__next > div {
+                    height: 100%;
+                }
+            `}</style>
       </React.Fragment>
     );
   }
