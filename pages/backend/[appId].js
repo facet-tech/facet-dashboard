7833cf174990f8e1aef223f4cd784cf1f7aef9be
mkotsollaris <mkotsollari@gmail.com>
2021-03-01 00:24:45 -0800

    added backwards navigation to [appId].js

--- pages/backend/[appId].js
+++ pages/backend/[appId].js
@@ -5,7 +5,10 @@ import { getApp, getBackendFacet } from '../../services/facetApiService.js';
 import ParserBackendService from '../../services/ParserBackendService';
 import BackendFacetCarousel from '../../shared/components/BackendFacetPanel/BackendFacetCarousel';
 import { useRouter } from 'next/router';
+import IconButton from "@material-ui/core/IconButton";
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 import BackendApplicationList from '../../shared/components/BackendApplicationList';
+import Link from 'next/link'
 
 const Backend = () => {
 
@@ -23,6 +26,15 @@ const Backend = () => {
     }, []);
 
     return <div>
+        <Link href={`/backend`}>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+            >
+                <KeyboardBackspaceIcon />
+            </IconButton>
+        </Link>
+
         <h2>Backend Applications</h2>
         <BackendFacetCarousel />
     </div>
