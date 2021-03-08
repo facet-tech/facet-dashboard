d5c612b92bf012c81544c755632125cd5d59e74c
mkotsollaris <mkotsollari@gmail.com>
2021-03-08 12:55:43 -0800

    save progress

--- components/PaginatedTable/index.tsx
+++ components/PaginatedTable/index.tsx
@@ -33,19 +33,19 @@ const PaginatedTable = () => {
     const endIndex = ((page - 1) * 10) + 10;
     const currDomains = domains?.slice(startIndex, endIndex) || [];
 
-    const totalDomains = Math.ceil(domains.length / 10);
+    const totalDomains = !domains || domains.length === 0 ? 0 : Math.ceil(domains.length / 10);
     const classes = useStyles();
 
     let element = totalDomains === 0 ? <div>
         <FacetLabel text="No Domains Found." />
     </div> : <div className={classes.root}>
-            <Pagination classes={{ ul: classes.ul }} page={page} onChange={handleChange} count={totalDomains} />
-            {currDomains?.map(domain => {
-                return <li style={{ color: color.white }}>
-                    {domain.domain}
-                </li>
-            })}
-        </div>
+        <Pagination classes={{ ul: classes.ul }} page={page} onChange={handleChange} count={totalDomains} />
+        {currDomains?.map(domain => {
+            return <li style={{ color: color.white }}>
+                {domain.domain}
+            </li>
+        })}
+    </div>
     return element
 }
 
