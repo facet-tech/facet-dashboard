d194768a3af9aab9ac5df006339acb071c7f523d
mkotsollaris <mkotsollari@gmail.com>
2021-03-23 17:28:02 -0700

    save progress

--- shared/components/ApplicationCard.tsx
+++ shared/components/ApplicationCard.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { dashboardColor, color, applicationStack } from '../constant'
 import FacetLabel from './FacetLabel'
 import Icon from '../components/Icon'
+import FacetIconButton from './FacetIconButton'
 
 const TitleGrid = styled.div`
+    display: grid;
     color: ${color.white};
     background-color: ${dashboardColor.gray};
     padding: .5rem;
     border-radius: .5rem .5rem 0 0;
+    grid-template-columns: 80% 20%;
+    align-items: center;
 `
 
 const MainGrid = styled.div`
-    width: 18rem;
+    width: 22rem;
+    :hover {
+        transform: scale(1.01);
+    }
 `
 
 const PermissionGrid = styled.div`
@@ -28,29 +35,44 @@ const DescriptionGrid = styled.div`
 `
 
 const StyledA = styled.a`
+
 `;
 
-const ApplicationCard = ({ name, appStack = applicationStack.java, isAuthorized = true, href = '' }) => {
-    return <StyledA href={href}>
+const ApplicationCard = ({ name, favorite = false, appStack = applicationStack.java, isAuthorized = true, href = '' }) => {
+    const [isFavorite, setIsFavorite] = useState(favorite);
+
+    return <>
         <MainGrid>
             <TitleGrid>
-                <FacetLabel text={name} />
+                <StyledA href={href}>
+                    <FacetLabel extraStyle={{ fontWeight: 'bold' }} text={name} />
+                </StyledA>
+                <div style={{
+                    justifySelf: 'end'
+                }}>
+                    <FacetIconButton
+                        key={name + isFavorite}
+                        onClick={() => { setIsFavorite(!isFavorite) }}
+                        name={isFavorite ? 'star' : 'star-outline'} />
+                </div>
             </TitleGrid>
-            <DescriptionGrid>
-                <div>
-                    <img src={`../images/${appStack.imgName}`} alt={appStack.name} />
-                </div>
-            </DescriptionGrid>
-            {isAuthorized ? <PermissionGrid>
-                <div>
-                    <Icon fill={dashboardColor.green} name='checkmark-circle-2-outline' />
-                </div>
-                <div>
-                    <FacetLabel color={dashboardColor.green} text='Authorized' />
-                </div>
-            </PermissionGrid> : null}
+            <StyledA href={href}>
+                <DescriptionGrid>
+                    <div>
+                        <img src={`../images/${appStack.imgName}`} alt={appStack.name} />
+                    </div>
+                </DescriptionGrid>
+                {isAuthorized ? <PermissionGrid>
+                    <div>
+                        <Icon iconHeight='18' iconWidth='18' fill={dashboardColor.green} name='checkmark-circle-2-outline' />
+                    </div>
+                    <div>
+                        <FacetLabel color={dashboardColor.green} text='Authorized' />
+                    </div>
+                </PermissionGrid> : null}
+            </StyledA>
         </MainGrid>
-    </StyledA>
+    </>
 }
 
 export default ApplicationCard;
--- shared/components/BackendApplicationList/index.tsx
+++ shared/components/BackendApplicationList/index.tsx
@@ -1,14 +1,13 @@
 import React, { useContext } from 'react';
 import AppContext from '../../../context/AppContext';
-import Link from 'next/link'
-import { color } from '../../constant';
 import ApplicationCard from '../ApplicationCard';
 import styled from 'styled-components';
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: 18rem 18rem;
+    grid-template-columns: 22rem 22rem;
     grid-gap: 1rem;
+    overflow: hidden;
 `;
 
 
