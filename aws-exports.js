91742b40c06e4bddb8b798c929fdb3cb1a8ed7bd
mkotsollaris <mkotsollari@gmail.com>
2021-04-13 17:20:02 -0700

    redirecting to /application post login

--- aws-exports.js
+++ aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/applications",
+        "redirectSignOut": "http://localhost:3000/applications",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS"
