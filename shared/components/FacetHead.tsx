15495a689467dad6dde6dc1cc3d6514d15ea83bd
mkotsollaris <mkotsollari@gmail.com>
2021-04-21 13:00:21 -0700

    updated FacetHead

--- shared/components/FacetHead.tsx
+++ shared/components/FacetHead.tsx
@@ -2,7 +2,19 @@ import Head from 'next/head'
 const FacetHead = () => <Head>
   <script src="https://api.facet.run/js?id=DOMAIN~OTU2ODRlZmMtMzg3Ni00N2NlLTkyZmUtYTg4NTU0ZGQyMTdi"></script>    {/* google analytics */}
 
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-LKCVL1Y70Q"></script>
+  {/* HotJar */}
+  <script dangerouslySetInnerHTML={{
+    __html: `(function(h,o,t,j,a,r){
+      h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+        h._hjSettings={hjid:2362799,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
+
+  {/* Google Analytics */}
+  < script async src="https://www.googletagmanager.com/gtag/js?id=G-LKCVL1Y70Q" ></script>
   <script dangerouslySetInnerHTML={{
     __html: `
         window.dataLayer = window.dataLayer || [];
